30d72df2b2936d1469878560a64674ee15465235
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-08-14 11:20:54 -0400

    feat(inputForm): search song using genius api

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Button, Card, CardSection, Input } from './common';
 
 class InputForm extends Component {
   state = { song: '' };
+
+  //this will turn the song being searched into a query format
+  querySong(song) {
+    const splitSong = song.split(' ');
+    return splitSong.join('%20');
+  }
+
+  onButtonPress() {
+    const { song } = this.state;
+    const queryString = this.querySong(song);
+    axios
+      .get(`https://genius.com/api/search?q=${queryString}`)
+      .then(res => res.data);
+  }
 
   render() {
     return (
@@ -11,11 +26,11 @@ class InputForm extends Component {
           <Input
             placeholder={'Enter song name'}
             value={this.state.song}
-            onChangesong={song => this.setState({ song })}
+            onChangeText={song => this.setState({ song })}
           />
         </CardSection>
         <CardSection>
-          <Button>Search</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Search</Button>
         </CardSection>
       </Card>
     );
