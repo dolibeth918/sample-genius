ea2ae913699583fd2637b29dcb22202885d43be1
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-08-18 18:15:49 -0400

    add(server): /login route for spotify user login

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const queryString = require('query-string');
 
 const redirect_uri =
-  process.env.REDIRECT_URI ||
-  'http://localhost:8888/callback';
+  process.env.REDIRECT_URI || 'http://localhost:8888/callback';
 
+app.get('/login', (req, res) => {
+  res.redirect(
+    'https://accounts.spotify.com/authorize?' +
+      queryString.stringify({
+        response_type: 'code',
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: 'user-read-private user-read-email',
+        redirect_uri
+      })
+  );
+});
