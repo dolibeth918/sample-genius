bfe918c0e9204d79ee5c5c6a57c742751b0dec7f
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-08-22 19:33:14 -0400

    add(store): add action creator and reducer to songs

--- src/redux/songs.js
+++ src/redux/songs.js
@@ -1,2 +1,14 @@
 /* -----------------    ACTION TYPES    ------------------ */
 const GET_SONGS = 'GET_SONGS';
+
+/* ------------     ACTION CREATORS      ------------------ */
+const getSongs = songs => ({ type: GET_SONGS, songs });
+
+/* ------------          REDUCER         ------------------ */
+
+export default function reducer(songs = [], action) {
+  if (action.type === 'GET_SONGS') {
+    return { songs: action.songs };
+  }
+  return songs;
+}
